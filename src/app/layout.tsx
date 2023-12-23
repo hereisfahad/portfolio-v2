8cdbce2d75d9d7def764eeb40fc0ef860b3966ee
Fahad Jabbar <fahadjabbar31@gmail.com>
2023-12-23 21:43:53 +0500

    feat: add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Inter } from "next/font/google";
 
 import "./globals.css";
@@ -40,6 +41,7 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
