026b5c6c66b7661ea4cac4868d816ce7c494f4ab
Fahad Jabbar <fahadjabbar31@gmail.com>
2023-12-25 18:03:32 +0500

    chore: remove vertical lines

--- src/components/landing/landing.tsx
+++ src/components/landing/landing.tsx
@@ -11,31 +11,11 @@ import useBlockRef from "@/hooks/useBlockRef";
 import "./home.css";
 
 const HomePage = () => {
-  // generate vertical lines from top to bottom on bg
-  const lines = [];
-  const colorClasses = ["bg_1", "bg_2", "bg_3", "bg_4"];
   const [width, setWidth] = useState(0);
 
   //Block position
   const blockRef = useRef(null);
   useBlockRef({ key: "landingPosition", blockRef });
-
-  for (let i = 0; i < 40; i++) {
-    lines.push(
-      <div
-        ref={blockRef}
-        className={`line ${colorClasses[Math.floor(Math.random() * 4)]}`}
-        key={i}
-        style={{
-          left: `${i * 3}%`,
-          animationDelay: `${Math.random() * 5}s`,
-          animationDuration: `${Math.random() * 5 + 5}s`,
-          marginLeft: `${Math.random() * 100}px`,
-          zIndex: 50,
-        }}
-      ></div>
-    );
-  }
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -51,7 +31,6 @@ const HomePage = () => {
   return (
     <div>
       <div className="flex flex-1 relative w-full h-screen overflow-hidden flex-col">
-        {lines}
         <div className="h-screen w-[100%] flex flex-col justify-center flex-1 max-w-[1000px] self-center mx-auto items-center text-center text-text_white z-100 relative">
           <div className="absolute top-[45%] left-0 flex flex-col gap-5 ml-5">
             <Link
